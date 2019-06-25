308a41525cb8ad8f13f1fc6f328608b7aa512f0d
MartijnDevGron <luub_82@hotmail.com>
2019-06-25 13:38:34 +0200

    menulijst uit restaurant object gehaald

--- backend/LowCalAPISolution/LowCalAPI/Controllers/RestaurantsController.cs
+++ backend/LowCalAPISolution/LowCalAPI/Controllers/RestaurantsController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LowCalAPI.Entities;
-using Newtonsoft.Json;
 
 
 namespace LowCalAPI.Controllers
