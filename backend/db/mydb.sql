ab192030d5dd7ae4424d1c32b4116293e68c71c0
MartijnDevGron <luub_82@hotmail.com>
2019-06-25 15:30:50 +0200

    nu dan wel goed?

--- backend/db/mydb.sql
+++ backend/db/mydb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `mydb` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mydb`;
 -- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: mydb
@@ -108,10 +110,6 @@ INSERT INTO `restaurant` VALUES (1,'Mechoui','Folkingestraat 38','9711JX','050-3
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'mydb'
---
-
---
 -- Dumping routines for database 'mydb'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -124,4 +122,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-25 15:16:41
+-- Dump completed on 2019-06-25 15:29:06
